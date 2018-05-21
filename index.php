698b86c2e403141c395bf4f5762a6f579707d7d7
doesntexist <32286853+doesntexist@users.noreply.github.com>
2018-05-21 14:59:48 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
  <?php
- echo "I am VERSION TWO ::: My Pod IP is ".$_SERVER['SERVER_ADDR']." ::: My URL is ".$_SERVER['SERVER_NAME']."";
+ echo "I am VERSION THREE ::: My Pod IP is ".$_SERVER['SERVER_ADDR']." ::: My URL is ".$_SERVER['SERVER_NAME']."";
 ?>
